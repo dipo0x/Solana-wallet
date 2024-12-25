6b0a1183f41a1c6917eb940cc213394e6be643f9
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2024-12-25 16:33:45 +0100

    added create wallet, send funds, get balance and transactions

---
+++ src/modules/wallet/wallet.controller.ts
@@ -0,0 +1,130 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import {
+    Connection,
+    PublicKey,
+    Keypair,
+    SystemProgram,
+    LAMPORTS_PER_SOL,
+    Transaction,
+  } from "@solana/web3.js";
+import dotenv from 'dotenv';
+import bs58 from 'bs58';
+dotenv.config();
+
+const QUICKNODE_RPC_URL = process.env.QUICKNODE_RPC_URL!
+const wallet = {
+    async createWallet(
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) {
+      try {
+    
+        const wallet = Keypair.generate();
+        // const connection = new Connection(QUICKNODE_RPC_URL, "confirmed");
+
+        return reply.code(200).send({
+            status: 200,
+            success: false,
+            message: {
+                publicAddress: wallet.publicKey.toString(),
+                privateAddress: Buffer.from(wallet.secretKey).toString("base64")
+            },
+        });
+
+    } catch (e) {
+        return reply.code(500).send({
+          status: 500,
+          success: false,
+          message: e,
+        });
+      }
+    },
+
+    async sendOut(
+        request: FastifyRequest<{
+            Body: {
+                sender_private_key: string;
+                recipent_public_key: string;
+                amount: number;
+            };
+        }>,
+        reply: FastifyReply
+    ) {
+      try {
+        const { sender_private_key, recipent_public_key, amount } = request.body
+        const connection = new Connection(process.env.SOLANA_DEVNET_API!, "confirmed");
+        
+        const privateKey = bs58.decode(sender_private_key);
+        if (privateKey.length !== 64) {
+            throw new Error("Invalid private key length");
+        }
+        const fromWallet = Keypair.fromSecretKey(privateKey);
+      
+        const recipientAddress = recipent_public_key
+      
+        const lamports = amount * LAMPORTS_PER_SOL;
+      
+        const transaction = new Transaction().add(
+          SystemProgram.transfer({
+            fromPubkey: fromWallet.publicKey,
+            toPubkey: new PublicKey(recipientAddress),
+            lamports,
+          })
+        );
+        const signature = await connection.sendTransaction(transaction, [fromWallet]);
+        await connection.confirmTransaction(signature, "confirmed");
+        
+        return reply.code(200).send({
+            status: 200,
+            success: true,
+            message: `Amount of SOL ${amount} has been sent`,
+          });
+      }
+      catch (e) {
+        console.log(e)
+        return reply.code(500).send({
+          status: 500,
+          success: false,
+          message: e,
+        });
+      }
+    },
+
+    async getBalanceWithTransactions(
+        request: FastifyRequest,
+        reply: FastifyReply
+    ) {
+      try {
+        const public_key = request.headers["public_key"];
+        const connection = new Connection(process.env.SOLANA_DEVNET_API!, "confirmed");
+
+        const publicKey = new PublicKey(public_key);
+
+        const balanceLamports = await connection.getBalance(publicKey);
+        const balanceSOL = balanceLamports / LAMPORTS_PER_SOL;
+
+        const confirmedSignatures = await connection.getSignaturesForAddress(publicKey, {
+            limit: 10,
+        });
+
+        return reply.code(200).send({
+            status: 200,
+            success: false,
+            message: {
+                balance: `${balanceSOL} SOL`,
+                transactions: confirmedSignatures
+            },
+          });
+
+    } catch (e) {
+        console.log(e)
+        return reply.code(500).send({
+          status: 500,
+          success: false,
+          message: e,
+        });
+      }
+    },
+}
+
+export default wallet
---
+++ src/modules/wallet/wallet.route.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify';
+import walletController from './wallet.controller';
+
+async function walletRoutes(server: FastifyInstance) {
+  server.get('/create-wallet', {
+    handler: walletController.createWallet
+  })
+  server.post('/send-sol', {
+    handler: walletController.sendOut
+  })
+  server.post('/get-balance-with-transactions', {
+    handler: walletController.sendOut
+  })
+  server.get('/retrieve-balance-with-transactions', {
+    handler: walletController.getBalanceWithTransactions
+  })
+}
+
+export default walletRoutes;
